38fcfca25c8a246ec7ef541b65062597e320f65d
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-08-02 17:29:39 +0200

    Fetch clients from server and display in listing.

--- App/Controllers/Backend/ClientController.js
+++ App/Controllers/Backend/ClientController.js
@@ -1,10 +1,16 @@
 'use strict'
 
+const Client = require('../../Models/').client;
+
 module.exports = {
 
     async index(req, res) {
         if (!req.xhr) {
             return res.render('backend/index', { title: 'T3', csrfToken: req.csrfToken() });
         }
+
+        const clients = await Client.all();
+
+        res.status(200).json({clients: clients});
     }
 }
--- resources/assets/js/app/components/clients/index.jsx
+++ resources/assets/js/app/components/clients/index.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
+import Ajax from '../../core/Helpers/AjaxHelper';
 
-const Clients = () => (
-    <div>
-        <table className='listing-table'>
-            <tbody>
-                <tr>
-                    <td>
-                        name
-                    </td>
-                    <td className='listing-td-actions'>
-                        actions
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        name
-                    </td>
-                    <td className='listing-td-actions'>
-                        actions
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-);
+class Clients extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clients: [],
+        }
+    }
+
+    componentDidMount() {
+
+        Ajax.get('/app/clients')
+            .then(res => {
+                this.setState({clients: res.clients});
+            })
+            .catch(err => console.log(err));
+    }
+
+    render() {
+
+        const clientRows = this.state.clients.map(c => 
+            <tr key={c.id} >
+                <td>
+                    {c.name}
+                </td>
+                <td className='listing-td-actions'>
+                    actions
+                </td>
+            </tr>
+        );
+        return (
+            <div>
+                <table className='listing-table'>
+                    <tbody>
+                        {clientRows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+};
+
+
 
 export default Clients;
